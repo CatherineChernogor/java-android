d17def8973899bc598b822a2587b933d0728d173
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-10-05 14:37:14 +0800

    add showing results

--- cities-square/src/com/company/Main.java
+++ cities-square/src/com/company/Main.java
@@ -1,26 +1,35 @@
 package com.company;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        Mansion m = new Mansion(0.45, 15, 10, 3);
-        System.out.println(m);
+
         Park p = new Park(0.75, 200, 300);
-        System.out.println(p);
+        //System.out.println(p);
         Theatre t = new Theatre(0.7, 30, 5);
-        System.out.println(t);
+        //System.out.println(t);
+        Mansion m = new Mansion(0.45, 15, 10, 3);
+        //System.out.println(m);
+
+        if (m.getArea() > t.getArea())
+            System.out.println("Mansion is bigger than Park");
+        else if (m.getArea() < t.getArea())
+            System.out.println("Park is bigger than Mansion");
+        else
+            System.out.println("Mansion and Park have equal price");
 
         ArrayList<Building> city = new ArrayList<Building>();
-        city.add(m);
         city.add(p);
         city.add(t);
+        city.add(m);
 
+        System.out.println('\n');
         System.out.println(city);
+        System.out.println('\n');
         Collections.sort(city);
         System.out.println(city);
-        
+
     }
 }
